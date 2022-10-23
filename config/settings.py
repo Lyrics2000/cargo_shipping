05c3bbfe23d3f7842590f3f2e518d726651b057e
Lyrics2000 <thomasambetsa@gmail.com>
2022-10-24 01:42:02 +0300

    ajj34

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-e2i$9+dpxp$qt!tgsat-tt+&a82wa7wj&w1gz$l=lad9opez6b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
